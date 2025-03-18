77756cf7a8a817be82d31321427f7907480d47f1
sumeyaahmed98 <sumeyaahmed@gmail.com>
2025-03-18 23:22:12 +0300

    done

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 function addingEventListener() {
-}
+    const button = document.getElementById("button"); // Make sure this element exists in your HTML
+    button.addEventListener("click", function () {
+      alert("Button clicked!");
+    });
+  }
+  
+  module.exports = addingEventListener; // Export function for testing
